8a0171da745f19321c21a410e6fea081a94cc7ab
Arav Agarwal <aravagarwal3073@gmail.com>
2020-09-18 14:31:43 -0400

    Remove temporary files

--- functions_to_add/test/src/rcpp_hello_world.cpp
+++ functions_to_add/test/src/rcpp_hello_world.cpp
@@ -4,11 +4,6 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 int rcpp_hello_world(NumericVector sources, NumericVector destinations, int num) {
-
-    
-    // Example Usage - Example taken from EecsforGeeks
-    vector<int> arc_src{ 0, 0, 0, 1, 1, 2, 2, 3, 4, 4, 6, 7};
-    vector<int> arc_targ{1, 3, 5, 2, 4, 6, 3, 7, 5, 6, 7, 5};
     arc_src = as<std::vector<int> >(sources);
     arc_targ = as<std::vector<int> >(destinations);
     return IsConnected(arc_src, arc_targ, num);
