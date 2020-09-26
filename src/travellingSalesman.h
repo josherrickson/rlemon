49eb42b65773d73723b44065d727722dacd4c56a
Arav Agarwal <aravagarwal3073@gmail.com>
2020-09-26 19:47:52 -0400

    Add all TSP except InsertionTSP

--- src/travellingSalesman.h
+++ src/travellingSalesman.h
@@ -2,21 +2,24 @@
 #include <iostream>
 #include <lemon/christofides_tsp.h>
 #include <lemon/greedy_tsp.h>
+#include <lemon/insertion_tsp.h>
+#include <lemon/nearest_neighbor_tsp.h>
+#include <lemon/opt2_tsp.h>
 
 typedef int Value;
 
 using namespace lemon;
 using namespace std;
 
-auto ChristofidesRunner(vector<int> arcSources, vector<int> arcTargets, vector<int> arcDistances, int numNodes) {
+auto ChristofidesRunner(vector<int> arcSources, vector<int> arcTargets, vector<int> arcDistances, int numNodes, int defaultEdgeWeight=999999) {
   // Requires: Two vectors, arcSources and arcTargets, each of which take integers to index specific nodes and, as pairs, consitute arcs in our graph
   //           One vector, arcDistances, which assigns for each arc an associated distance
   //           Two ints, numNodes and startnode, which give us the number of nodes in the directed graph and the starting node for Bellman Ford
   // Returns: One vector, which contains the minimum distances from the start node to each of the nodes, with "-1" used as a placeholder to indicates the target and source and disjoint
-  // ATTENTION: TSP implemented using fullgraph, so remember that we can't test this for large n. 
+  // ATTENTION: TSP implemented using fullgraph, so remember that all edges exist
   FullGraph g(numNodes);
 
-  FullGraph::EdgeMap<int> costs(g);
+  FullGraph::EdgeMap<int> costs(g,defaultEdgeWeight);
 
   int NUM_ARCS = arcSources.size();
 
@@ -35,7 +38,7 @@ auto ChristofidesRunner(vector<int> arcSources, vector<int> arcTargets, vector<i
   return runner.tourCost();
 }
 
-auto GreedyTSPRunner(vector<int> arcSources, vector<int> arcTargets, vector<int> arcDistances, int numNodes) {
+auto GreedyTSPRunner(vector<int> arcSources, vector<int> arcTargets, vector<int> arcDistances, int numNodes, int defaultEdgeWeight=999999) {
   // Requires: Two vectors, arcSources and arcTargets, each of which take integers to index specific nodes and, as pairs, consitute arcs in our graph
   //           One vector, arcDistances, which assigns for each arc an associated distance
   //           Two ints, numNodes and startnode, which give us the number of nodes in the directed graph and the starting node for Bellman Ford
@@ -43,7 +46,7 @@ auto GreedyTSPRunner(vector<int> arcSources, vector<int> arcTargets, vector<int>
   // ATTENTION: TSP implemented using fullgraph, so remember that we can't test this for large n. 
   FullGraph g(numNodes);
 
-  FullGraph::EdgeMap<int> costs(g);
+  FullGraph::EdgeMap<int> costs(g,defaultEdgeWeight);
 
   int NUM_ARCS = arcSources.size();
 
@@ -53,6 +56,87 @@ auto GreedyTSPRunner(vector<int> arcSources, vector<int> arcTargets, vector<int>
 
   
   GreedyTsp<FullGraph::EdgeMap<int>> runner(g,costs);
+  runner.run();
+
+  for(int i = 0; i < NUM_ARCS; ++i) {
+      std::cout << g.id(runner.tourNodes()[i]) << std::endl;
+  }
+
+  return runner.tourCost();
+}
+/*
+auto InsertionTSPRunner(vector<int> arcSources, vector<int> arcTargets, vector<int> arcDistances, int numNodes, int defaultEdgeWeight=999999) {
+  // Requires: Two vectors, arcSources and arcTargets, each of which take integers to index specific nodes and, as pairs, consitute arcs in our graph
+  //           One vector, arcDistances, which assigns for each arc an associated distance
+  //           Two ints, numNodes and startnode, which give us the number of nodes in the directed graph and the starting node for Bellman Ford
+  // Returns: One vector, which contains the minimum distances from the start node to each of the nodes, with "-1" used as a placeholder to indicates the target and source and disjoint
+  // ATTENTION: TSP implemented using fullgraph, so remember that we can't test this for large n. 
+  FullGraph g(numNodes);
+
+  FullGraph::EdgeMap<int> costs(g,defaultEdgeWeight);
+
+  int NUM_ARCS = arcSources.size();
+
+  for(int i = 0; i < NUM_ARCS; ++i) {
+      costs[g.arc(g(arcSources[i]),g(arcTargets[i]))] = arcDistances[i];
+  }
+
+  
+  InsertionTsp<FullGraph::EdgeMap<int>> runner(g,costs);
+  runner.run();
+
+  for(int i = 0; i < NUM_ARCS; ++i) {
+      std::cout << g.id(runner.tourNodes()[i]) << std::endl;
+  }
+
+  return runner.tourCost();
+}
+*/
+auto NearestNeighborTSPRunner(vector<int> arcSources, vector<int> arcTargets, vector<int> arcDistances, int numNodes, int defaultEdgeWeight=999999) {
+  // Requires: Two vectors, arcSources and arcTargets, each of which take integers to index specific nodes and, as pairs, consitute arcs in our graph
+  //           One vector, arcDistances, which assigns for each arc an associated distance
+  //           Two ints, numNodes and startnode, which give us the number of nodes in the directed graph and the starting node for Bellman Ford
+  // Returns: One vector, which contains the minimum distances from the start node to each of the nodes, with "-1" used as a placeholder to indicates the target and source and disjoint
+  // ATTENTION: TSP implemented using fullgraph, so remember that we can't test this for large n. 
+  FullGraph g(numNodes);
+
+  FullGraph::EdgeMap<int> costs(g,defaultEdgeWeight);
+
+  int NUM_ARCS = arcSources.size();
+
+  for(int i = 0; i < NUM_ARCS; ++i) {
+      costs[g.arc(g(arcSources[i]),g(arcTargets[i]))] = arcDistances[i];
+  }
+
+  
+  NearestNeighborTsp<FullGraph::EdgeMap<int>> runner(g,costs);
+  runner.run();
+
+  for(int i = 0; i < NUM_ARCS; ++i) {
+      std::cout << g.id(runner.tourNodes()[i]) << std::endl;
+  }
+
+  return runner.tourCost();
+}
+
+auto Opt2TSPRunner(vector<int> arcSources, vector<int> arcTargets, vector<int> arcDistances, int numNodes, int defaultEdgeWeight=999999) {
+  // Requires: Two vectors, arcSources and arcTargets, each of which take integers to index specific nodes and, as pairs, consitute arcs in our graph
+  //           One vector, arcDistances, which assigns for each arc an associated distance
+  //           Two ints, numNodes and startnode, which give us the number of nodes in the directed graph and the starting node for Bellman Ford
+  // Returns: One vector, which contains the minimum distances from the start node to each of the nodes, with "-1" used as a placeholder to indicates the target and source and disjoint
+  // ATTENTION: TSP implemented using fullgraph, so remember that we can't test this for large n. 
+  FullGraph g(numNodes);
+
+  FullGraph::EdgeMap<int> costs(g,defaultEdgeWeight);
+
+  int NUM_ARCS = arcSources.size();
+
+  for(int i = 0; i < NUM_ARCS; ++i) {
+      costs[g.arc(g(arcSources[i]),g(arcTargets[i]))] = arcDistances[i];
+  }
+
+  
+  Opt2Tsp<FullGraph::EdgeMap<int>> runner(g,costs);
   runner.run();
 
   for(int i = 0; i < NUM_ARCS; ++i) {
