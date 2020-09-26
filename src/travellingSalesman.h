0a038a7cb9eb6e52358f9956426c22e53b9827cb
Arav Agarwal <aravagarwal3073@gmail.com>
2020-09-26 19:30:25 -0400

    Add GreedyTSP

--- src/travellingSalesman.h
+++ src/travellingSalesman.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include <iostream>
 #include <lemon/christofides_tsp.h>
+#include <lemon/greedy_tsp.h>
 
 typedef int Value;
 
@@ -25,6 +26,33 @@ auto ChristofidesRunner(vector<int> arcSources, vector<int> arcTargets, vector<i
 
   
   ChristofidesTsp<FullGraph::EdgeMap<int>> runner(g,costs);
+  runner.run();
+
+  for(int i = 0; i < NUM_ARCS; ++i) {
+      std::cout << g.id(runner.tourNodes()[i]) << std::endl;
+  }
+
+  return runner.tourCost();
+}
+
+auto GreedyTSPRunner(vector<int> arcSources, vector<int> arcTargets, vector<int> arcDistances, int numNodes) {
+  // Requires: Two vectors, arcSources and arcTargets, each of which take integers to index specific nodes and, as pairs, consitute arcs in our graph
+  //           One vector, arcDistances, which assigns for each arc an associated distance
+  //           Two ints, numNodes and startnode, which give us the number of nodes in the directed graph and the starting node for Bellman Ford
+  // Returns: One vector, which contains the minimum distances from the start node to each of the nodes, with "-1" used as a placeholder to indicates the target and source and disjoint
+  // ATTENTION: TSP implemented using fullgraph, so remember that we can't test this for large n. 
+  FullGraph g(numNodes);
+
+  FullGraph::EdgeMap<int> costs(g);
+
+  int NUM_ARCS = arcSources.size();
+
+  for(int i = 0; i < NUM_ARCS; ++i) {
+      costs[g.arc(g(arcSources[i]),g(arcTargets[i]))] = arcDistances[i];
+  }
+
+  
+  GreedyTsp<FullGraph::EdgeMap<int>> runner(g,costs);
   runner.run();
 
   for(int i = 0; i < NUM_ARCS; ++i) {
