5d937c548e1ad6d3668cce33377563a9a919ec5c
Arav Agarwal <aravagarwal3073@gmail.com>
2020-10-02 14:22:33 -0400

    Add min cost flow test code

--- src/min_cost_flow.h
+++ src/min_cost_flow.h
@@ -1,12 +1,12 @@
-#include <Rcpp.h>
 #include <vector>
 
-#include "lemon/min_cost_flow_NS.h"
-#include "lemon/min_cost_flow_CS.h"
+#include <lemon/cost_scaling.h>
+using namespace std;
 
-using namespace Rcpp;
+using namespace lemon;
 
-// [[Rcpp::export]]
+
+/*
 std::vector<int> minFlow(std::vector<int> node_supplies, std::vector<int> arc_src, std::vector<int> arc_targ, std::vector<int> arc_costs, std::vector<int> arc_capacities)
 {
     std::vector<Flow> outflow;
@@ -15,4 +15,33 @@ std::vector<int> minFlow(std::vector<int> node_supplies, std::vector<int> arc_sr
     else outflow = networkSimplex(node_supplies, arc_src, arc_targ, arc_costs, arc_capacities);
 
     return outflow;
+}
+*/
+
+auto CapacityScalingRunner(vector<int> arcSources, vector<int> arcTargets, vector<int> arcDistances, vector<int> arcSupplies, int numNodes) {
+    ListDigraph g;
+    vector<ListDigraph::Node> nodes;
+    for(int i = 0; i < numNodes; ++i) {
+        ListDigraph::Node n = g.addNode();
+        nodes.push_back(n);
+    }
+    
+    vector<ListDigraph::Arc> arcs;
+    int NUM_ARCS = arcSources.size();
+
+    ListDigraph::ArcMap<int> dists(g);
+    ListDigraph::ArcMap<int> supplies(g);
+    for(int i = 0; i < NUM_ARCS; ++i) {
+        ListDigraph::Arc a = g.addArc(nodes[arcSources[i]], nodes[arcTargets[i]]);
+        dists[a] = arcDistances[i];
+        supplies[a] = arcSupplies[i];
+        arcs.push_back(a);
+    }
+
+
+    CostScaling<ListDigraph, int, int> runner(g);
+    runner.costMap(dists);
+    runner.supplyMap(dists);
+    runner.run();
+    return runner.totalCost();
 }
