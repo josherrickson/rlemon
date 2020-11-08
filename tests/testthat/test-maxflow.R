1aaf99c5eb983ebf3941f104a4bcdface65f3ca8
Arav Agarwal <AravAgar@umich.edu>
2020-11-08 17:16:09 -0500

    Fix-maxflow

--- tests/testthat/test-maxflow.R
+++ tests/testthat/test-maxflow.R
@@ -15,7 +15,6 @@ test_that("maxflow works", {
   t <- c(1,2,2,3,1,4,2,5,3,5)
   cap <- c(16,13,10,124,14,9,20,7,4)
   out <- EdmondsKarpRunner(s,t,cap,0,5,6)
-  print(out)
   expect_equal(out[[3]],7)
   expect_equal(out[[2]], c(1,1,1,1,1,0))
   expect_equal(out[[1]], c(7,0,0,7,0,0,0,7,0,0))
