86855061bca1e51fff8a2acb14bfbf8851019ac1
Arav Agarwal <AravAgar@umich.edu>
2020-11-09 01:09:28 -0500

    Finish planarity tests

--- tests/testthat/test-planarity.R
+++ tests/testthat/test-planarity.R
@@ -5,3 +5,23 @@ test_that("Planarity Checking", {
   expect_is(out, "logical")
   expect_equal(out, FALSE)
 })
+
+test_that("Planar-Coloring Checking", {
+  s <- c(0,0,0,1,1,1,2)
+  t <- c(3,4,5,3,4,5,0)
+  out <- PlanarColoringRunner(s,t,6)
+  expect_is(out[[1]], "logical")
+  expect_equal(out[[1]], TRUE)
+  expect_equal(out[[2]], c(1,1,0,0,0,0))
+})
+
+
+test_that("Planar-Embedding Checking", {
+  s <- c(0,0,0,1,1,1,2)
+  t <- c(3,4,5,3,4,5,0)
+  out <- PlanarDrawingRunner(s,t,6)
+  expect_is(out[[1]], "logical")
+  expect_equal(out[[1]], TRUE)
+  expect_equal(out[[2]], c(2,1,1,4,2,0))
+  expect_equal(out[[3]], c(2,0,2,1,1,4))
+})
