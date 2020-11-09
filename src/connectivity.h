3eeb30df36adf695e92de379ff02035998cd30a4
Arav Agarwal <AravAgar@umich.edu>
2020-11-09 13:02:32 -0500

    Fixed build error on documentation

--- src/connectivity.h
+++ src/connectivity.h
@@ -7,6 +7,24 @@
 #include <Rcpp.h>
 using namespace lemon;
 
+//' Graph Input Specifications
+//' @name Input-Information
+//' @description Due to how LEMON operates, and to make the interfaces similar to the actual program, 
+//' graphs in rLEMON use the following 2 vectors:
+//' arcSources, and arcTargets
+//' to acquire information about how the graph is structured
+//' each edge in a graph will consist of a (node id, node id) pair, which is represented
+//' by the ith entry in both arcSources and arcTargets. 
+//' The nodes are 0-indexed, and must be sequential. 
+//' numNodes must be equal to the number of nodes in the graph, as otherwise there
+//' are potentials to error out. 
+//' As an example, the cycle graph on three vertices would have the following input:
+//' arcSources <- c(0,1,2)
+//' arcTargets <- c(1,2,0)
+//' numNodes <- 3
+//' Here, the edges are (0,1), (1,2), (2,0)
+//> NULL
+
 //' Connectivity Algorithms - Boolean Return
 //' @name Connectivity-Algorithms-1
 //' @param arcSources, a vector corresponding to the source nodes of a graph's edges
