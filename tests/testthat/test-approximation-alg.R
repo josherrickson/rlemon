7b21ae0d528bc7e529b3379c7de6f17768c60d9e
Arav Agarwal <AravAgar@umich.edu>
2020-11-09 13:25:46 -0500

    Add Grosso-Locatelli Pullan Tests

---
+++ tests/testthat/test-approximation-alg.R
@@ -0,0 +1,8 @@
+test_that("grosso locatelli works", {
+  s <- c(0,1,2,3,4)
+  t <- c(1,2,0,4,3)
+  n <- 5
+  out <- GrossoLocatelliPullanMcRunner(s,t,n)
+  expect_equal(out[[1]],3)
+  expect_equal(out[[2]],c(0,1,2))
+})
