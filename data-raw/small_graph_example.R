f117ca7090546b3f809a8f432e242bb9a7d97052
Josh Errickson <josh@errickson.net>
2021-03-23 14:24:55 -0400

    Disabling 0-indexing
    Data example was manually converted to 0-indexing; we've moved back to 1-indexing

--- data-raw/small_graph_example.R
+++ data-raw/small_graph_example.R
@@ -1,7 +1,6 @@
 small_graph_example <- list(
-  # "- 1"'s are to enable base-0 vectors for C
-  startnodes = read.csv("small_graph_startnodes.csv", header = TRUE)$x - 1,
-  endnodes   = read.csv("small_graph_endnodes.csv",   header = TRUE)$x - 1,
+  startnodes = read.csv("small_graph_startnodes.csv", header = TRUE)$x,
+  endnodes   = read.csv("small_graph_endnodes.csv",   header = TRUE)$x,
   arccosts = read.csv("small_graph_arccosts.csv", header = TRUE)$x,
   arccapacity = read.csv("small_graph_arccapacity.csv", header = TRUE)$x,
   nodedemand = read.csv("small_graph_nodedemand.csv", header = TRUE)$x,
