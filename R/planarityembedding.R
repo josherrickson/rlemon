a9a8ae7e94a1dc5cfc55dafd20a502c0a3c1168b
Arav Agarwal <aravagar@umich.edu>
2022-03-08 15:54:13 -0500

    Update planer embedding docs

--- R/planarityembedding.R
+++ R/planarityembedding.R
@@ -1,4 +1,6 @@
 ##' Checks if an undirected graph is planar.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00617.html#ga230242aa2ee36f9b1b5a58f2c53016eb'
+##' for more information.
 ##'
 ##' @title PlanarChecking
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -17,6 +19,8 @@ PlanarChecking <- function(arcSources,
 
 ##' Checks if an undirected graph is planar and returns a list of outputs
 ##' related to the planar embedding
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00308.html'
+##' for more information.
 ##'
 ##' @title PlanarEmbedding
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -37,6 +41,8 @@ PlanarEmbedding <- function(arcSources,
 }
 
 ##' Checks if a graph is planar and returns the coloring of the graph
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00306.html'
+##' for more information.
 ##'
 ##' @title PlanarColoring
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -68,6 +74,8 @@ PlanarColoring <- function(arcSources,
 }
 
 ##' Checks if a graph is planar and returns the coloring of the graph
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00307.html'
+##' for more information.
 ##'
 ##' @title PlanarDrawing
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
