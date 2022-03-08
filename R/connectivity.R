353295bfe300764b76e5fa23cbbc1c89885a4b6c
Arav Agarwal <aravagar@umich.edu>
2022-03-08 15:51:39 -0500

    Update connectivity docs

--- R/connectivity.R
+++ R/connectivity.R
@@ -1,5 +1,7 @@
-##' Checks if an undirected graph is bipartite and returns the bipartite
-##' partitions
+##' Checks if an undirected graph is bipartite and finds the bipartite
+##' partitions.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga58ba1d00c569f0eb0deb42afca9f80bb'
+##' for more information.
 ##'
 ##' @title GetBipartitePartitions
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -20,7 +22,9 @@ GetBipartitePartitions <- function(arcSources,
   )
 }
 
-##' Checks if a directed graph is a DAG and returns the topological order
+##' Checks if a directed graph is a DAG and returns the topological order.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gafc2cb20cf3859f157c0e12da7f310bb3'
+##' for more information.
 ##'
 ##' @title GetAndCheckTopologicalSort
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -42,7 +46,9 @@ GetAndCheckTopologicalSort <- function(arcSources,
   )
 }
 
-##' Gives back the topological order of a DAG
+##' Gives back the topological order of a DAG.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gafc2cb20cf3859f157c0e12da7f310bb3'
+##' for more information.
 ##'
 ##' @title GetTopologicalSort
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -60,7 +66,9 @@ GetTopologicalSort <- function(arcSources,
   .Call(`_rlemon_getTopologicalSortRunner`, arcSources, arcTargets, numNodes)
 }
 
-##' Checks if an undirected graph is connected
+##' Checks if an undirected graph is connected.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gad5c8d1b650f6b614a852f8430d90e184'
+##' for more information.
 ##'
 ##' @title IsConnected
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -77,7 +85,9 @@ IsConnected <- function(arcSources,
   .Call(`_rlemon_IsConnectedRunner`, arcSources, arcTargets, numNodes)
 }
 
-##' Checks if an undirected graph is acyclic
+##' Checks if an undirected graph is acyclic.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga14c191b2133a1dd23e1527f074c821c0'
+##' for more information.
 ##'
 ##' @title IsAcylic
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -94,7 +104,9 @@ IsAcyclic <- function(arcSources,
   .Call(`_rlemon_IsAcyclicRunner`, arcSources, arcTargets, numNodes)
 }
 
-##' Checks if an undirected graph is a tree
+##' Checks if an undirected graph is a tree.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gad1e4de234e926958647905478415bd54'
+##' for more information.
 ##'
 ##' @title IsTree
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -111,7 +123,9 @@ IsTree <- function(arcSources,
   .Call(`_rlemon_IsTreeRunner`, arcSources, arcTargets, numNodes)
 }
 
-##' Checks if an undirected graph is bipartite
+##' Checks if an undirected graph is bipartite.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga577db110d33bd487aaad5bfffb31c6f5'
+##' for more information.
 ##'
 ##' @title IsBipartite
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -128,7 +142,9 @@ IsBipartite <- function(arcSources,
   .Call(`_rlemon_IsBipartiteRunner`, arcSources, arcTargets, numNodes)
 }
 
-##' Checks if a directed graph is strongly connected
+##' Checks if a directed graph is strongly connected.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gacd21b34d7b42b9835a204a57fcf15964'
+##' for more information.
 ##'
 ##' @title IsStronglyConnected
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -145,7 +161,9 @@ IsStronglyConnected <- function(arcSources,
   .Call(`_rlemon_IsStronglyConnectedRunner`, arcSources, arcTargets, numNodes)
 }
 
-##' Checks if a directed graph is a DAG
+##' Checks if a directed graph is a DAG.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gaef2b43c8cd1d74e15fa5c7607bc5e396'
+##' for more information.
 ##'
 ##' @title IsDAG
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -162,7 +180,9 @@ IsDAG <- function(arcSources,
   .Call(`_rlemon_IsDAGRunner`, arcSources, arcTargets, numNodes)
 }
 
-##' Checks if an undirected graph is bi-node connected
+##' Checks if an undirected graph is bi-node connected.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga37d22a2ddd5a064a9203720f2b93518e'
+##' for more information.
 ##'
 ##' @title IsBiNodeConnected
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -179,7 +199,9 @@ IsBiNodeConnected <- function(arcSources,
   .Call(`_rlemon_IsBiNodeConnectedRunner`, arcSources, arcTargets, numNodes)
 }
 
-##' Checks if an undirected graph is bi-edge connected
+##' Checks if an undirected graph is bi-edge connected.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga37d22a2ddd5a064a9203720f2b93518e'
+##' for more information.
 ##'
 ##' @title IsBiEdgeConnected
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -198,6 +220,8 @@ IsBiEdgeConnected <- function(arcSources,
 
 ##' Checks if a graph is loop free. Works for both undirected and directed
 ##' graphs.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga127f3963003cd532c79c226885fe1c8c'
+##' for more information.
 ##'
 ##' @title IsLoopFree
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -216,6 +240,8 @@ IsLoopFree <- function(arcSources,
 
 ##' Checks if a graph has no parallel arcs/edges. Works for both undirected and
 ##' directed graphs.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gaa05e0683f90b69f31eb29fe7d09afde4'
+##' for more information.
 ##'
 ##' @title IsParallelFree
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -234,6 +260,8 @@ IsParallelFree <- function(arcSources,
 
 ##' Checks if a graph is simple, i.e. it contains no loop arcs/edges and no
 ##' parallel arcs/edges. Works for both undirected and directed graphs.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gae4c7ae734e2509ab78dc747d602c9236'
+##' for more information.
 ##'
 ##' @title IsSimpleGraph
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -252,6 +280,8 @@ IsSimpleGraph <- function(arcSources,
 
 ##' Checks if a graph is Eulerian. Works for both undirected and directed
 ##' graphs.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gafb5a4961cac4d877006869fc4cb6ea1d'
+##' for more information.
 ##'
 ##' @title IsEulerian
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -269,6 +299,8 @@ IsEulerian <- function(arcSources,
 }
 
 ##' Counts the number of bi-edge connected components in an undirected graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gaf7c5744b2175210b8ea67897aaa27885'
+##' for more information.
 ##'
 ##' @title CountBiEdgeConnectedComponents
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -289,6 +321,8 @@ CountBiEdgeConnectedComponents <- function(arcSources,
 }
 
 ##' Counts the number of connected components in an undirected graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga33a9d9d4803cb15e83568b2526e978a5'
+##' for more information.
 ##'
 ##' @title CountConnectedComponents
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -309,6 +343,8 @@ CountConnectedComponents <- function(arcSources,
 }
 
 ##' Counts the number of bi-node connected components in an undirected graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gaf7c5744b2175210b8ea67897aaa27885'
+##' for more information.
 ##'
 ##' @title CountBiNodeConnectedComponents
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -329,6 +365,8 @@ CountBiNodeConnectedComponents <- function(arcSources,
 }
 
 ##' Counts the number of strongly connected components in a directed graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gad30bc47dfffb78234eeee903cb3766f4'
+##' for more information.
 ##'
 ##' @title CountStronglyConnectedComponents
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -349,6 +387,8 @@ CountStronglyConnectedComponents <- function(arcSources,
 }
 
 ##' Finds the strongly connected components in a directed graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga46f8c22f3e2989c4689faa4c46ec9436'
+##' for more information.
 ##'
 ##' @title FindStronglyConnectedComponents
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -369,6 +409,8 @@ FindStronglyConnectedComponents <- function(arcSources,
 }
 
 ##' Finds the cut arcs of strongly connected components in a directed graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gad7af5c3a97453e37f251f0e86dbb83db'
+##' for more information.
 ##'
 ##' @title FindStronglyConnectedCutArcs
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -390,6 +432,8 @@ FindStronglyConnectedCutArcs <- function(arcSources,
 }
 
 ##' Finds the bi-edge-connected cut edges in an undirected graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga58d444eba448c5f1a53539bd1b69636e'
+##' for more information.
 ##'
 ##' @title FindBiEdgeConnectedCutEdges
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -411,6 +455,8 @@ FindBiEdgeConnectedCutEdges <- function(arcSources,
 }
 
 ##' Finds the bi-node-connected components of an undirected graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga9d70526ab54e10b4b6fe3762af8675dd'
+##' for more information.
 ##'
 ##' @title FindBiNodeConnectedComponents
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -431,6 +477,8 @@ FindBiNodeConnectedComponents <- function(arcSources,
 }
 
 ##' Finds the bi-node-connected cut nodes in an undirected graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga31461f33a748327ea3ef2a3199ffb6c7'
+##' for more information.
 ##'
 ##' @title FindBiNodeConnectedCutNodes
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -451,6 +499,8 @@ FindBiNodeConnectedCutNodes <- function(arcSources,
 }
 
 ##' Finds the connected components in an undirected graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#gaa467a3e0a8c2e9e762650fd01fadff89'
+##' for more information.
 ##'
 ##' @title FindConnectedComponents
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
@@ -471,6 +521,8 @@ FindConnectedComponents <- function(arcSources,
 }
 
 ##' Finds the bi-edge-connected components of an undirected graph.
+##' See 'http://lemon.cs.elte.hu/pub/doc/1.3.1/a00616.html#ga76c1fdd1881d21677507100b7e96c983'
+##' for more information.
 ##'
 ##' @title FindBiEdgeConnectedComponents
 ##' @param arcSources Vector corresponding to the source nodes of a graph's
