bf034198fd59adba19866540381ef9a9fb5b9950
Josh Errickson <josh@errickson.net>
2022-03-15 12:56:34 -0400

    Expand some tests

--- tests/testthat/test-connectivity.R
+++ tests/testthat/test-connectivity.R
@@ -1,119 +1,152 @@
-test_that("Testing connectivity", {
+# Connectivity
+# Runners: IsConnectedRunner, IsAcyclicRunner, IsTreeRunner, IsBipartiteRunner,
+#   IsStronglyConnectedRunner, IsDAGRunner, IsBiNodeConnectedRunner,
+#   IsBiEdgeConnectedRunner, IsLoopFreeRunner, IsParallelFreeRunner,
+#   IsSimpleGraphRunner, IsEulerianRunner, getBipartitePartitionsRunner,
+#   getAndCheckTopologicalSortRunner, getTopologicalSortRunner,
+#   CountBiEdgeConnectedComponentsRunner, CountConnectedComponentsRunner,
+#   CountBiNodeConnectedComponentsRunner,
+#   CountStronglyConnectedComponentsRunner,
+#   FindStronglyConnectedComponentsRunner, FindStronglyConnectedCutArcsRunner,
+#   FindBiEdgeConnectedCutEdgesRunner, FindBiNodeConnectedComponentsRunner,
+#   FindBiNodeConnectedCutNodesRunner, FindConnectedComponentsRunner,
+#   FindBiEdgeConnectedComponentsRunner
+# Exported: GetBipartitePartitions, GetAndCheckTopologicalSort,
+#   GetTopologicalSort, IsConnected, IsAcyclic, IsTree, IsBipartite,
+#   IsStronglyConnected, IsDAG, IsBiNodeConnected, IsBiEdgeConnected,
+#   IsLoopFree, IsParallelFree, IsSimpleGraph, IsEulerian,
+#   CountBiEdgeConnectedComponents, CountConnectedComponents,
+#   CountBiNodeConnectedComponents, ountStronglyConnectedComponents,
+#   FindStronglyConnectedComponents, FindStronglyConnectedCutArcs,
+#   FindBiEdgeConnectedCutEdges, FindBiNodeConnectedComponents,
+#   FindBiNodeConnectedCutNodes, FindConnectedComponents,
+#   FindBiEdgeConnectedComponents
+# Valid Algorithms: No algorithms in functions
+
+### Is___ functions
+
+# 1) Ensure runner functions run without error and return the "expected
+# objects".
+test_that("Testing Is____ runners", {
   s <- c(1, 2, 3, 4)
   t <- c(2, 3, 4, 1)
   n <- 4
+
   out <- IsConnected(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 1)
-})
+  expect_true(out %in% 0:1)
 
-test_that("Testing acyclicity", {
-  s <- c(1, 2, 3, 4)
-  t <- c(2, 3, 4, 1)
-  n <- 4
   out <- IsAcyclic(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 0)
-})
+  expect_true(out %in% 0:1)
 
-
-test_that("Testing tree", {
-  s <- c(1, 1, 1)
-  t <- c(2, 3, 4)
-  n <- 4
   out <- IsTree(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 1)
-})
+  expect_true(out %in% 0:1)
 
-
-
-test_that("Testing biparite", {
-  s <- c(1, 1, 1)
-  t <- c(2, 3, 4)
-  n <- 4
   out <- IsBipartite(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 1)
-})
+  expect_true(out %in% 0:1)
 
-
-test_that("Testing DAG", {
-  s <- c(1, 1, 1)
-  t <- c(2, 3, 4)
-  n <- 4
   out <- IsDAG(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 1)
-})
+  expect_true(out %in% 0:1)
 
-
-
-test_that("Testing Bi-Node Connectiveness", {
-  s <- c(1, 1, 1)
-  t <- c(2, 3, 4)
-  n <- 4
   out <- IsBiNodeConnected(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 0)
-})
+  expect_true(out %in% 0:1)
 
-test_that("Testing Bi-Edge Connectiveness", {
-  s <- c(1, 1, 1)
-  t <- c(2, 3, 4)
-  n <- 4
   out <- IsBiEdgeConnected(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 0)
-})
+  expect_true(out %in% 0:1)
 
-
-test_that("Testing Loop Free", {
-  s <- c(1, 1, 1)
-  t <- c(1, 3, 4)
-  n <- 4
   out <- IsLoopFree(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 0)
-})
+  expect_true(out %in% 0:1)
 
-
-test_that("Testing parallel-free", {
-  s <- c(1, 1, 1)
-  t <- c(2, 3, 3)
-  n <- 3
   out <- IsParallelFree(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 0)
-})
+  expect_true(out %in% 0:1)
 
-test_that("simple graph check works", {
-  s <- c(1, 2, 3)
-  t <- c(2, 3, 1)
-  n <- 3
   out <- IsSimpleGraph(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 1)
-})
+  expect_true(out %in% 0:1)
 
-
-test_that("eulerian check works", {
-  s <- c(1, 2, 3)
-  t <- c(2, 3, 1)
-  n <- 3
   out <- IsEulerian(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 1)
-})
+  expect_true(out %in% 0:1)
 
-test_that("strongly connected check works", {
-  s <- c(1, 2, 3)
-  t <- c(2, 3, 1)
-  n <- 3
   out <- IsStronglyConnected(s, t, n)
   expect_is(out, "integer")
-  expect_equal(out, 1)
+  expect_true(out %in% 0:1)
+
+  out <- getBipartitePartitionsRunner(s, t, n)
+  expect_is(out, "list")
+  expect_length(out, 2)
+  expect_true(is.logical(out[[1]]))
+  expect_true(is.numeric(out[[2]]))
+
+  out <- getAndCheckTopologicalSortRunner(s, t, n)
+  expect_is(out, "list")
+  expect_length(out, 2)
+  expect_true(is.logical(out[[1]]))
+  expect_true(is.numeric(out[[2]]))
+
+  out <- getTopologicalSortRunner(s, t, n)
+  expect_is(out, "integer")
+
+  out <- CountBiEdgeConnectedComponentsRunner(s, t, n)
+  expect_is(out, "integer")
+  expect_length(out, 1)
+  expect_true(out >= 0)
+
+  out <- CountConnectedComponentsRunner(s, t, n)
+  expect_is(out, "integer")
+  expect_length(out, 1)
+  expect_true(out >= 0)
+
+  out <- CountBiNodeConnectedComponentsRunner(s, t, n)
+  expect_is(out, "integer")
+  expect_length(out, 1)
+  expect_true(out >= 0)
+
+  out <- CountStronglyConnectedComponentsRunner(s, t, n)
+  expect_is(out, "integer")
+  expect_length(out, 1)
+  expect_true(out >= 0)
+
+  out <- FindStronglyConnectedComponentsRunner(s, t, n)
+  expect_is(out, "integer")
+
+  out <- FindStronglyConnectedCutArcsRunner(s, t, n)
+  expect_is(out, "list")
+  expect_length(out, 2)
+  expect_true(is.numeric(out[[1]]))
+  expect_true(is.numeric(out[[2]]))
+
+  out <- FindBiEdgeConnectedCutEdgesRunner(s, t, n)
+  expect_is(out, "list")
+  expect_length(out, 2)
+  expect_true(is.numeric(out[[1]]))
+  expect_true(is.numeric(out[[2]]))
+
+  out <- FindBiNodeConnectedComponentsRunner(s, t, n)
+  expect_is(out, "integer")
+
+  out <- FindBiNodeConnectedCutNodesRunner(s, t, n)
+  expect_is(out, "integer")
+
+  out <- FindConnectedComponentsRunner(s, t, n)
+  expect_is(out, "integer")
+
+  out <- FindBiEdgeConnectedComponentsRunner(s, t, n)
+  expect_is(out, "integer")
 })
+
+
+
+
+
 
 
 test_that("countConnectedComponents works", {
